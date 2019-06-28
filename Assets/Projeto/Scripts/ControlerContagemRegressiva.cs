6089e4125d47bd0d4ddd112d657161df97ff47d1
Elias <elias.oliveira.laguna@gmail.com>
2019-06-27 22:07:51 -0300

    contagem regressiva para terminar a fase

---
+++ Assets/Projeto/Scripts/ControlerContagemRegressiva.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlerContagemRegressiva : MonoBehaviour {
+
+	public float _tempoInicial = 120;
+	public Text _contadorRegressivo;
+
+	public string tempoZerado;
+
+	// Use this for initialization
+	void Start () {
+
+		_contadorRegressivo.text = _tempoInicial.ToString();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(_tempoInicial >= 0){
+
+			_tempoInicial -= Time.deltaTime;
+			_contadorRegressivo.text = Mathf.Round(_tempoInicial).ToString();
+
+			if(_tempoInicial <= 0){
+				SceneManager.LoadScene(tempoZerado);
+			}
+
+		}
+		
+	}
+}
